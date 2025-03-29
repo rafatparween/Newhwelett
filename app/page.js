8fa8d2be0473191aa075fa50f93217e4e1a0609b
rafat22 <rafatparween27392@gmail.com>
2025-03-30 02:21:35 +0530

    initial commit

---
+++ app/page.js
@@ -0,0 +1,27 @@
+import DataContent from "./Components/DataContent";
+import Footer from "./Components/Footer";
+import Navbar from "./Components/Navbar";
+import Slider from "./Components/Slider";
+import Support from "./Components/Support";
+import JivoChat from "./JivoChat";
+
+export default function Home() {
+  return (
+    <>
+      {/* <Navbar/>
+      <Slider/>
+     
+      <Support/>
+      <DataContent/>
+      <Footer/>
+      <JivoChat/> */}
+      <Navbar/>
+      <Slider/>
+      <Support/>
+      <DataContent/>
+      <Footer/>
+      <JivoChat/>
+     
+    </>
+  );
+}
