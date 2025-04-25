7916e046a82608fe522f408af41dfdaab210d581
rafatparween <rafatparween27392@gmail.com>
2025-04-25 19:33:11 +0530

    add google ads

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,42 @@
+// import { Geist, Geist_Mono } from "next/font/google";
+// import "./globals.css";
+
+
+
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
+
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
+
+// export const metadata = {
+//   title: " Uzair Graphic & Printing",
+//   description: " Uzair Graphic & Printing",
+// };
+
+// export default function RootLayout({ children }) {
+//   return (
+//     <html lang="en">
+//       <body
+//         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+//       >
+//         {/* <Navbar /> Navbar should be rendered before children */}
+//         <main>{children}</main> {/* Main content area for the children */}
+//          {/* Footer should be at the bottom of the page */}
+//       </body>
+//     </html>
+//   );
+// }
+
+
+
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
-
-
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -14,19 +49,34 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata = {
-  title: " Uzair Graphic & Printing",
-  description: " Uzair Graphic & Printing",
+  title: "Uzair Graphic & Printing",
+  description: "Uzair Graphic & Printing",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Ads - gtag.js script */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=AW-16967997482"
+          strategy="afterInteractive"
+        />
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-16967997482');
+          `}
+        </Script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {/* <Navbar /> Navbar should be rendered before children */}
-        <main>{children}</main> {/* Main content area for the children */}
-         {/* Footer should be at the bottom of the page */}
+        {/* <Navbar /> */}
+        <main>{children}</main>
+        {/* Footer */}
       </body>
     </html>
   );
